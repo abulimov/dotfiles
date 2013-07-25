37ae2f12561ff36d21894c1b9e1e4cca50fc7a32
Alexander Bulimov <abulimov@credberry.ru>
2013-07-25 13:01:55 +0400

    first normal version

---
+++ README.md
@@ -0,0 +1,16 @@
+## dotfiles
+
+The way I personalize my OS.
+
+## install
+
+Run this:
+
+```sh
+git clone https://github.com/abulimov/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+~/dotfiles/bootstrap.sh
+```
+## thanks
+
+I forked [Zach Holman](http://github.com/holman)' dotfiles and tweaked them to suit my needs.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# bootstrap dotfiles
 
-ROOT=$(pwd)
+declare -r ROOT=`dirname $(readlink -f $0)`
+
+zsh_theme="lazywolf"
 
 info () {
   printf " [ \033[00;34m..\033[0m ] $1\n"
@@ -18,6 +22,31 @@ fail () {
   printf "\r\033[2K [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
+}
+
+
+setup_gitconfig () {
+  if ! [ -f "$ROOT/git/gitconfig.symlink" ]
+  then
+    info 'setup gitconfig'
+
+    git_credential='cache'
+    if [ "$(uname -s)" == "Darwin" ]
+    then
+      git_credential='osxkeychain'
+    fi
+
+    user ' - What is your github author name?'
+    read -e git_authorname
+    user ' - What is your github author email?'
+    read -e git_authoremail
+
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
+
+    success 'gitconfig'
+  else
+    info 'found gitconfig'
+  fi
 }
 
 link_files () {
@@ -72,35 +101,118 @@ install_oh_my_zsh() {
 install_my_zsh_theme() {
   theme="$1"
   src="$ROOT/zsh/$theme.zsh-theme"
-  target="$HOME/.oh-my-zsh/themes/$theme.zsh-theme"
+  dest="$HOME/.oh-my-zsh/themes/$theme.zsh-theme"
   if [ ! -f "$src" ]; then
     fail "no source file $src"
     exit
   fi
   if [ -d "$HOME/.oh-my-zsh/themes" ]; then
     info "found ~/.oh-my-zsh/themes"
-    if [ -f "$target" ]; then
-      out=$(diff "$src" "$target" 2>&1)
-      echo $out
-      result=$?
-      if [ 0 -eq "$result" ]; then
-        info "found correct zsh theme"
-      elif [ 1 -eq "$result" ]; then
-        rm "$target"
-        link_files "$src" "$target"
-      else 
-        fail "diff returned error with message $out"
+    if [ -f "$dest" ]; then
+      user "File already exists: `basename $src`, what do you want to do? [s]kip, [o]verwrite, [b]ackup?"
+      read -n 1 action
+
+      case "$action" in
+        o )
+          overwrite=true;;
+        b )
+          backup=true;;
+        s )
+          skip=true;;
+        * )
+          ;;
+      esac
+      if [ "$overwrite" == "true" ] 
+      then
+        rm -rf $dest
+        success "removed $dest"
+      fi
+
+      if [ "$backup" == "true" ]
+      then
+        mv $dest $dest\.backup
+        success "moved $dest to $dest.backup"
+      fi
+
+      if [ "$skip" == "false" ]
+      then
+        link_files $src $dest
+      else
+        success "skipped $src"
       fi
     else
-      link_files "$src" "$target"
+      link_files "$src" "$dest"
     fi
   fi
 }
+
+install_dotfiles () {
+  info 'installing dotfiles'
+  overwrite_all=false
+  backup_all=false
+  skip_all=false
+
+  for src in `find $ROOT -maxdepth 2 -name \*.symlink`
+  do
+    dest="$HOME/.`basename \"${src%.*}\"`"
+
+    if [ -f $dest ] || [ -d $dest ]
+    then
+      overwrite=false
+      backup=false
+      skip=false
+
+      if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+      then
+        user "File already exists: `basename $src`, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+      fi
+
+      if [ "$overwrite" == "true" ] || [ "$overwrite_all" == "true" ]
+      then
+        rm -rf $dest
+        success "removed $dest"
+      fi
+
+      if [ "$backup" == "true" ] || [ "$backup_all" == "true" ]
+      then
+        mv $dest $dest\.backup
+        success "moved $dest to $dest.backup"
+      fi
+
+      if [ "$skip" == "false" ] && [ "$skip_all" == "false" ]
+      then
+        link_files $src $dest
+      else
+        success "skipped $src"
+      fi
+    else
+      link_files $src $dest
+    fi
+done
+}
+
 install_oh_my_zsh
 switch_to_zsh
-install_my_zsh_theme "lazywolf"
-#install_vim_files
-#link_dotfiles
+install_my_zsh_theme "$zsh_theme"
+install_dotfiles
 
-echo ''
+echo 'done!'
 
